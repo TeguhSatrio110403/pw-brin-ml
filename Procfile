ad1496fdb000a87951320819a7b3508a02f4e3fc
Rio Mulya Syawal <riomulya75@gmail.com>
2025-05-25 16:35:44 +0700

    revert

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn --preload --workers 1 "main:create_app()" 
+web: gunicorn wsgi:app 
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-from flask import Flask, jsonify
+from flask import Flask
 from app.routes.predict import predict_bp
-import os
 
 def create_app():
     app = Flask(__name__)
@@ -8,11 +7,6 @@ def create_app():
     # Register blueprint dengan prefix /api
     app.register_blueprint(predict_bp, url_prefix='/api')
 
-    # Tambahkan health check endpoint untuk mencegah Render spinning down
-    @app.route('/health')
-    def health():
-        return jsonify({"status": "ok"}), 200
-        
     # Tambahkan route sederhana buat tes di browser
     @app.route('/')
     def index():
@@ -22,4 +16,4 @@ def create_app():
 
 if __name__ == "__main__":
     app = create_app()
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=False)
+    app.run(debug=True)
