546d72d582df1482dd269d82a6fb5e74f0d86ff1
TeguhSatrio110403 <152392220+TeguhSatrio110403@users.noreply.github.com>
2025-06-28 11:37:31 +0700

    update

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,16 @@
 from flask import Flask
 from .config import Config
 from .routes.predict import api
+from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
     app.register_blueprint(api)
+
+    CORS(app, origins=["https://pw-brin-main.vercel.app", "http://localhost:5173"])
+    # Untuk production, ganti "*" dengan domain frontend kamu, misal:
+    # CORS(app, origins=["https://namadomainfrontend.com"])
 
     @app.route("/")
     def index():
